48ac88a0673aa81a634d42aa78190c04c2e8152d
its-id <ik.dana24@gmail.com>
2024-03-31 21:41:15 +0530

    format db files

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client'
-import bcrypt from "bcrypt";
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+const prisma = new PrismaClient();
 
 async function main() {
   const alice = await prisma.user.upsert({
@@ -12,21 +12,21 @@ async function main() {
       name: 'alice',
       Balance: {
         create: {
-            amount: 20000,
-            locked: 0
-        }
+          amount: 20000,
+          locked: 0,
+        },
       },
       OnRampTransaction: {
         create: {
           startTime: new Date(),
-          status: "Success",
+          status: 'Success',
           amount: 20000,
-          token: "token__1",
-          provider: "HDFC Bank",
+          token: 'token__1',
+          provider: 'HDFC Bank',
         },
       },
     },
-  })
+  });
   const bob = await prisma.user.upsert({
     where: { number: '2222222222' },
     update: {},
@@ -36,29 +36,29 @@ async function main() {
       name: 'bob',
       Balance: {
         create: {
-            amount: 2000,
-            locked: 0
-        }
+          amount: 2000,
+          locked: 0,
+        },
       },
       OnRampTransaction: {
         create: {
           startTime: new Date(),
-          status: "Failure",
+          status: 'Failure',
           amount: 2000,
-          token: "token__2",
-          provider: "HDFC Bank",
+          token: 'token__2',
+          provider: 'HDFC Bank',
         },
       },
     },
-  })
-  console.log({ alice, bob })
+  });
+  console.log({ alice, bob });
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
